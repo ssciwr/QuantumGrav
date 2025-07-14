d74b54cf140621503ebb79aa8b34faca6743ae42
Harald Mack <harald.mack@protonmail.com>
2025-07-14 17:10:50 +0200

    work on julia interface

--- QuantumGravPy/src/QuantumGrav/julia_interface.py
+++ QuantumGravPy/src/QuantumGrav/julia_interface.py
@@ -5,59 +5,53 @@ import logging
 
 
 class JuliaInterface:
-    """Interface for interacting with the Julia programming language. This class provides methods to initialize the QuantumGrav.jl package, add directories to the Julia load path, and execute Julia code from Python."""
+    package_path: Path | str | None = None
+    module = None
+    logger = logging.getLogger(__name__)
+    custom_modules: dict[str, Path] | None = None
 
     def __init__(
         self, package_path: str | Path | None = None, log_level: int = logging.INFO
     ):
-        """Initialize the Julia interface for QuantumGrav.
-
-        Args:
-            package_path (str | Path | None, optional): Path to the QuantumGrav.jl package. Defaults to None.
-            log_level (int, optional): Logging level. Defaults to logging.INFO.
-        """
         # set up package name
         if package_path is None:
+            # README: this seems brittle -> how to do better? -> use installation of quantumgrav locally and use the module path from there?
             self.package_path = (
                 Path(os.path.abspath(__file__)).parent.parent.parent / "QuantumGrav.jl"
             )
+            self.add_to_load_path(self.package_path)
         else:
             self.package_path = Path(os.path.abspath(package_path))
         self.module = jcall.newmodule("QuantumGravFromPy")
-        self._initialize_quantumgrav_julia()
-        self.logger = logging.getLogger(__name__)
+        self._initialize_quantumgrav_module()
         self.logger.setLevel(log_level)
+        self.custom_modules = {}
 
     def add_to_load_path(self, path: str | Path) -> None:
-        """
-        Add a directory to the Julia load path.
-        """
         path = Path(path).resolve()
-        jcall.seval(f'push!(LOAD_PATH, "{path}")')
+        self.module.seval(f'push!(LOAD_PATH, "{path}")')
         self.logger.info(f"Added {path} to Julia load path.")
 
     def load_custom_module(self, path: str | Path) -> None:
-        """
-        Load a custom Julia module from the specified path.
-        """
         path = Path(path).resolve()
         self.add_to_load_path(path)
         module_name = path.stem
-        self.module.seval(f"using {module_name}")
+        self.custom_modules[module_name] = jcall.newmodule(module_name)
 
     def _initialize_quantumgrav_module(
         self,
     ) -> None:
-        """Initialize the Julia interface for QuantumGrav.
-
-        Raises:
-            RuntimeError: If initialization fails, e.g., if the package is not found.
-        """
         try:
             self.add_to_load_path(self.package_path)
 
+            # README: this uses the development version of the damn thing, because we don´t have any releases yet
+            setup_command = f"""
+            using Pkg
+            Pkg.develop(path="{self.package_path}/QuantumGrav.jl")
+            """
+            print(f"evaluating {setup_command}")
             # activate local environment
-            jcall.seval('using Pkg; Pkg.activate("$(self.package_path)")')
+            self.module.seval(setup_command)
 
             # load module
             self.module.seval("using QuantumGrav")
@@ -71,5 +65,4 @@ class JuliaInterface:
 
     @property
     def quantumgrav_jl_module(self):
-        """Get the Julia module object for interacting with QuantumGrav.jl"""
         return self.module
