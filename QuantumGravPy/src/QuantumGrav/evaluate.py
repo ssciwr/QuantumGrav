4e990888ab7ba6fd955ff97a09fa1c4af9ec0450
Harald Mack <harald.mack@protonmail.com>
2025-07-30 16:55:28 +0200

    add docstrings to evaluate

--- QuantumGravPy/src/QuantumGrav/evaluate.py
+++ QuantumGravPy/src/QuantumGrav/evaluate.py
@@ -1,11 +1,18 @@
 import torch
-from typing import Callable
+from typing import Callable, Any
 import torch_geometric
 from numpy import mean, std
 
 
 class DefaultEvaluator:
     def __init__(self, device, criterion: Callable, apply_model: Callable):
+        """Default evaluator for model evaluation.
+
+        Args:
+            device (_type_): The device to run the evaluation on.
+            criterion (Callable): The loss function to use for evaluation.
+            apply_model (Callable): A function to apply the model to the data.
+        """
         self.criterion = criterion
         self.apply_model = apply_model
         self.device = device
@@ -13,7 +20,16 @@ class DefaultEvaluator:
 
     def evaluate(
         self, model: torch.nn.Module, data_loader: torch_geometric.loader.DataLoader
-    ):
+    ) -> list[Any]:
+        """Evaluate the model on the given data loader.
+
+        Args:
+            model (torch.nn.Module): Model to evaluate.
+            data_loader (torch_geometric.loader.DataLoader): Data loader for evaluation.
+
+        Returns:
+             list[Any]: A list of evaluation results.
+        """
         model.eval()
         current_data = []
 
@@ -30,18 +46,36 @@ class DefaultEvaluator:
         return current_data
 
     def report(self, losses):
+        """Report the evaluation results to stdout"""
         avg = mean(losses)
         sigma = std(losses)
+        print(f"Average loss: {avg}, Standard deviation: {sigma}")
         self.data.append((avg, sigma))
 
 
 class DefaultTester(DefaultEvaluator):
     def __init__(self, device, criterion: Callable, apply_model: Callable):
+        """Default tester for model testing.
+
+        Args:
+            device (_type_): The device to run the testing on.
+            criterion (Callable): The loss function to use for testing.
+            apply_model (Callable): A function to apply the model to the data.
+        """
         super().__init__(device, criterion, apply_model)
 
     def test(
         self, model: torch.nn.Module, data_loader: torch_geometric.loader.DataLoader
     ):
+        """Test the model on the given data loader.
+
+        Args:
+            model (torch.nn.Module): Model to test.
+            data_loader (torch_geometric.loader.DataLoader): Data loader for testing.
+
+        Returns:
+            list[Any]: A list of testing results.
+        """
         return self.evaluate(model, data_loader)
 
 
@@ -52,4 +86,12 @@ class DefaultValidator(DefaultEvaluator):
     def validate(
         self, model: torch.nn.Module, data_loader: torch_geometric.loader.DataLoader
     ):
+        """Validate the model on the given data loader.
+
+        Args:
+            model (torch.nn.Module): Model to validate.
+            data_loader (torch_geometric.loader.DataLoader): Data loader for validation.
+        Returns:
+            list[Any]: A list of validation results.
+        """
         return self.evaluate(model, data_loader)
