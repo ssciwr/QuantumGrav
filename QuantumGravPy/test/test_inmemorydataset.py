fc15baecbe5de6b57ab43857333fa24129be61f0
Harald Mack <harald.mack@protonmail.com>
2025-08-13 14:32:56 +0200

    update inmemory dataset to support zarr

--- QuantumGravPy/test/test_inmemorydataset.py
+++ QuantumGravPy/test/test_inmemorydataset.py
@@ -7,12 +7,13 @@ from torch_geometric.data import Data
 
 
 @pytest.mark.parametrize("n", [1, 3], ids=["sequential", "parallel"])
-def test_inmemory_dataset_creation_and_process(create_data, read_data, n):
-    datadir, datafiles = create_data
+def test_inmemory_dataset_creation_and_process(create_data_hdf5, read_data, n):
+    datadir, datafiles = create_data_hdf5
 
     dataset = QG.QGDatasetInMemory(
         input=datafiles,
         output=datadir,
+        mode="hdf5",
         reader=read_data,
         float_type=torch.float32,
         int_type=torch.int64,
@@ -40,17 +41,50 @@ def test_inmemory_dataset_creation_and_process(create_data, read_data, n):
     assert isinstance(dataset[5], Data)
 
 
-def test_ondisk_dataset_with_dataloader(create_data, read_data):
-    datadir, datafiles = create_data
+@pytest.mark.parametrize("n", [1, 2], ids=["sequential", "parallel"])
+def test_ondisk_dataset_with_dataloader_hdf5(create_data_hdf5, read_data, n):
+    datadir, datafiles = create_data_hdf5
 
     dataset = QG.QGDatasetInMemory(
         input=datafiles,
         output=datadir,
+        mode="hdf5",
         reader=read_data,
         float_type=torch.float32,
         int_type=torch.int64,
         validate_data=True,
-        n_processes=1,
+        n_processes=n,
+        chunksize=4,
+        transform=lambda x: x,
+        pre_transform=lambda x: x,
+        pre_filter=lambda x: True,
+    )
+
+    loader = DataLoader(
+        dataset,
+        batch_size=2,
+        shuffle=True,
+    )
+    assert len(loader) == 8  # Assuming 15 samples and batch size of 2
+
+    for i, batch in enumerate(loader):
+        assert isinstance(batch, Data)
+        assert len(batch) == 2 if i < 7 else 1  # Last batch may be smaller
+
+
+@pytest.mark.parametrize("n", [1, 2], ids=["sequential", "parallel"])
+def test_ondisk_dataset_with_dataloader_zarr(create_data_zarr, read_data, n):
+    datadir, datafiles = create_data_zarr
+
+    dataset = QG.QGDatasetInMemory(
+        input=datafiles,
+        output=datadir,
+        mode="zarr",
+        reader=read_data,
+        float_type=torch.float32,
+        int_type=torch.int64,
+        validate_data=True,
+        n_processes=n,
         chunksize=4,
         transform=lambda x: x,
         pre_transform=lambda x: x,
