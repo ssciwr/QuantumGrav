cef9079a701ca754f8dbfdf3b22a8ab8b18ddc4e
Harald Mack <harald.mack@protonmail.com>
2025-10-17 11:36:57 +0200

    add name lookup from type

--- QuantumGravPy/src/QuantumGrav/utils.py
+++ QuantumGravPy/src/QuantumGrav/utils.py
@@ -8,14 +8,32 @@ gnn_layers: dict[str, type[torch.nn.Module]] = {
     "gcn": tgnn.conv.GCNConv,
     "gat": tgnn.conv.GATConv,
     "sage": tgnn.conv.SAGEConv,
-    "gco": tgnn.conv.GraphConv,
+    "gconv": tgnn.conv.GraphConv,
+    "gatconv2": tgnn.conv.GATv2Conv,
+}
+
+gnn_layers_names: dict[type[torch.nn.Module], str] = {
+    tgnn.conv.GCNConv: "gcn",
+    tgnn.conv.GATConv: "gat",
+    tgnn.conv.SAGEConv: "sage",
+    tgnn.conv.GraphConv: "gconv",
+    tgnn.conv.GATv2Conv: "gatconv2",
 }
 
 normalizer_layers: dict[str, type[torch.nn.Module]] = {
     "identity": torch.nn.Identity,
     "batch_norm": torch.nn.BatchNorm1d,
     "layer_norm": torch.nn.LayerNorm,
+    "graph_norm": tgnn.norm.GraphNorm,
 }
+
+normalizer_layers_names: dict[type[torch.nn.Module], str] = {
+    torch.nn.Identity: "identity",
+    torch.nn.BatchNorm1d: "batch_norm",
+    torch.nn.LayerNorm: "layer_norm",
+    tgnn.norm.GraphNorm: "graph_norm",
+}
+
 
 activation_layers: dict[str, type[torch.nn.Module]] = {
     "relu": torch.nn.ReLU,
@@ -25,11 +43,27 @@ activation_layers: dict[str, type[torch.nn.Module]] = {
     "identity": torch.nn.Identity,
 }
 
+activation_layers_names: dict[type[torch.nn.Module], str] = {
+    torch.nn.ReLU: "relu",
+    torch.nn.LeakyReLU: "leaky_relu",
+    torch.nn.Sigmoid: "sigmoid",
+    torch.nn.Tanh: "tanh",
+    torch.nn.Identity: "identity",
+}
+
+
 pooling_layers: dict[str, Callable | type[torch.nn.Module]] = {
     "mean": tgnn.global_mean_pool,
     "max": tgnn.global_max_pool,
     "sum": tgnn.global_add_pool,
     "identity": torch.nn.Identity,
+}
+
+pooling_layers_names: dict[type[torch.nn.Module] | Callable, str] = {
+    tgnn.global_mean_pool: "mean",
+    tgnn.global_max_pool: "max",
+    tgnn.global_add_pool: "sum",
+    torch.nn.Identity: "identity",
 }
 
 pooling_aggregations: dict[str, Callable | type[torch.nn.Module]] = {
@@ -38,10 +72,22 @@ pooling_aggregations: dict[str, Callable | type[torch.nn.Module]] = {
     "identity": torch.nn.Identity,
 }
 
+pooling_aggregations_names: dict[type[torch.nn.Module] | Callable, str] = {
+    torch.cat: "cat0",
+    partial(torch.cat, dim=1): "cat1",
+    torch.nn.Identity: "identity",
+}
+
 graph_features_aggregations: dict[str, Callable | type[torch.nn.Module]] = {
     "cat0": torch.cat,
     "cat1": partial(torch.cat, dim=1),
     "identity": torch.nn.Identity,
+}
+
+graph_features_aggregations_names: dict[type[torch.nn.Module] | Callable, str] = {
+    torch.cat: "cat0",
+    partial(torch.cat, dim=1): "cat1",
+    torch.nn.Identity: "identity",
 }
 
 
@@ -95,6 +141,7 @@ def register_pooling_layer(
     if pooling_layer_name in pooling_layers:
         raise ValueError(f"Pooling layer '{pooling_layer_name}' is already registered.")
     pooling_layers[pooling_layer_name] = pooling_layer
+    pooling_layers_names[pooling_layer] = pooling_layer_name
 
 
 def register_gnn_layer(gnn_layer_name: str, gnn_layer: type[torch.nn.Module]) -> None:
@@ -107,6 +154,7 @@ def register_gnn_layer(gnn_layer_name: str, gnn_layer: type[torch.nn.Module]) ->
     if gnn_layer_name in gnn_layers:
         raise ValueError(f"GNN layer '{gnn_layer_name}' is already registered.")
     gnn_layers[gnn_layer_name] = gnn_layer
+    gnn_layers_names[gnn_layer] = gnn_layer_name
 
 
 def register_normalizer(
@@ -124,6 +172,7 @@ def register_normalizer(
     if normalizer_name in normalizer_layers:
         raise ValueError(f"Normalizer '{normalizer_name}' is already registered.")
     normalizer_layers[normalizer_name] = normalizer_layer
+    normalizer_layers_names[normalizer_layer] = normalizer_name
 
 
 def register_activation(
@@ -141,6 +190,7 @@ def register_activation(
     if activation_name in activation_layers:
         raise ValueError(f"Activation '{activation_name}' is already registered.")
     activation_layers[activation_name] = activation_layer
+    activation_layers_names[activation_layer] = activation_name
 
 
 def register_graph_features_aggregation(
@@ -160,6 +210,7 @@ def register_graph_features_aggregation(
             f"Graph features aggregation '{aggregation_name}' is already registered."
         )
     graph_features_aggregations[aggregation_name] = aggregation_function
+    graph_features_aggregations_names[aggregation_function] = aggregation_name
 
 
 def register_pooling_aggregation(
@@ -179,6 +230,7 @@ def register_pooling_aggregation(
             f"Pooling aggregation '{aggregation_name}' is already registered."
         )
     pooling_aggregations[aggregation_name] = aggregation_function
+    pooling_aggregations_names[aggregation_function] = aggregation_name
 
 
 def get_registered_pooling_layer(name: str) -> type[torch.nn.Module] | Callable | None:
