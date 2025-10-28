e632b0486a32844321b815f7e015f2ab30370507
Fabian Wagner <fabianwagner94@live.de>
2025-10-28 12:49:11 +0100

    - simplified the calculation of n1 in insert_KR_into_manifoldlike

--- QuantumGrav.jl/src/csetmerging.jl
+++ QuantumGrav.jl/src/csetmerging.jl
@@ -202,13 +202,14 @@ function insert_KR_into_manifoldlike(
 )::Tuple{CausalSets.BitArrayCauset,Bool,Matrix{T}} where {T}
 
     n2_rel <= 0 && throw(ArgumentError("n2_rel must be larger than 0, is $n2_rel."))
-
-    n1 = max(1, round(Int, (1 - n2_rel) * npoints)) # Ensure at least 1
     
-    cset1Raw, _, _ =
-        make_polynomial_manifold_cset(n1, rng, order, r; d = d, type = type)
     n2 = max(3, round(Int, n2_rel * npoints))  # Ensure at least 3
 
+    n1 = max(1, npoints - n2) # Ensure at least 1
+
+    cset1Raw, _, _ =
+        make_polynomial_manifold_cset(n1, rng, order, r; d = d, type = type)
+        
     cset2Raw, _ = create_random_layered_causet(n2, 3; p = p)
 
     return insert_cset(
