42368f9b9e94d9fc66f39fbca7cabf1a171100e8
mephistoteles-whatever <fabianwagner94@live.de>
2025-10-30 12:52:53 +0100

    Bugfix for merging csets (#77)
    * - changed minimal number of points in KR order to 3 when merging
    with manifoldlike; this is necessary by consistency (3 layers require 3 points)
    
    * - modified one comment
    
    * - simplified the calculation of n1 in insert_KR_into_manifoldlike
    
    * - changed from at least 3 to at least 4 elements of KR order
    to ensure convergence

--- QuantumGrav.jl/src/csetmerging.jl
+++ QuantumGrav.jl/src/csetmerging.jl
@@ -202,12 +202,13 @@ function insert_KR_into_manifoldlike(
 )::Tuple{CausalSets.BitArrayCauset,Bool,Matrix{T}} where {T}
 
     n2_rel <= 0 && throw(ArgumentError("n2_rel must be larger than 0, is $n2_rel."))
-
-    n1 = max(1, round(Int, (1 - n2_rel) * npoints)) # Ensure at least 1
     
+    n2 = max(4, round(Int, n2_rel * npoints))  # Ensure at least 4
+
+    n1 = max(1, npoints - n2) # Ensure at least 1
+
     cset1Raw, _, _ =
         make_polynomial_manifold_cset(n1, rng, order, r; d = d, type = type)
-    n2 = max(1, round(Int, n2_rel * npoints))  # Ensure at least 1
 
     cset2Raw, _ = create_random_layered_causet(n2, 3; p = p)
 
