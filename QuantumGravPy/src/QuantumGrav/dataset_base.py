581c557cc3e6012b4c295ae8eacacbb91d469f8b
Harald Mack <harald.mack@protonmail.com>
2025-11-10 14:05:18 +0100

    remove hdf5 from dataset_base

--- QuantumGravPy/src/QuantumGrav/dataset_base.py
+++ QuantumGravPy/src/QuantumGrav/dataset_base.py
@@ -11,6 +11,7 @@ import zarr
 from pathlib import Path
 from collections.abc import Callable, Collection
 from joblib import delayed, Parallel
+from typing import Sequence
 
 
 class QGDatasetBase:
@@ -20,7 +21,6 @@ class QGDatasetBase:
         self,
         input: list[str | Path],
         output: str | Path,
-        mode: str = "hdf5",
         reader: Callable[
             [h5py.File | zarr.Group, torch.dtype, torch.dtype, bool],
             list[Data],
@@ -38,7 +38,6 @@ class QGDatasetBase:
         Args:
             input (list[str  |  Path] : The list of input files for the dataset, or a callable that generates a set of input files.
             output (str | Path): The output directory where processed data will be stored.
-            mode (str): File storage mode. 'zarr' or 'hdf5'
             reader (Callable[[h5py.File | zarr.Group, torch.dtype, torch.dtype, bool], list[Data]] | None, optional): A function to load data from a file. Defaults to None.
             float_type (torch.dtype, optional): The data type to use for floating point values. Defaults to torch.float32.
             int_type (torch.dtype, optional): The data type to use for integer values. Defaults to torch.int64.
@@ -54,10 +53,6 @@ class QGDatasetBase:
         if reader is None:
             raise ValueError("A reader function must be provided to load the data.")
 
-        if mode not in ["hdf5", "zarr"]:
-            raise ValueError("mode must be 'hdf5' or 'zarr'")
-
-        self.mode = mode
         self.input = input
         for file in self.input:
             if Path(file).exists() is False:
@@ -136,55 +131,36 @@ class QGDatasetBase:
                 return max_shape
 
         # same logic for Zarr and HDF5
-        if self.mode == "hdf5":
-            with h5py.File(filepath, "r") as f:
-                try:
-                    # note that fallback returns an int directly,
-                    # while for try_find_numsamples we need to index into the result
-                    s = try_find_numsamples(f)
-                    if s is not None:
-                        return s[()]
-                    else:
-                        s = fallback(f)
-                        if s is not None:
-                            return s
-                        else:
-                            raise RuntimeError("Unable to determine number of samples.")
-                except Exception:
-                    raise
-        elif self.mode == "zarr":
-            try:
-                group = zarr.open_group(
-                    zarr.storage.LocalStore(filepath, read_only=True),
-                    path="",
-                    mode="r",
-                )
-                # note that fallback returns an int directly,
-                # while for try_find_numsamples we need to index into the result
-                s = try_find_numsamples(group)
+        try:
+            group = zarr.open_group(
+                zarr.storage.LocalStore(filepath, read_only=True),
+                path="",
+                mode="r",
+            )
+            # note that fallback returns an int directly,
+            # while for try_find_numsamples we need to index into the result
+            s = try_find_numsamples(group)
+            if s is not None:
+                return s[0]
+            else:
+                s = fallback(group)
                 if s is not None:
-                    return s[0]
-                else:
-                    s = fallback(group)
-                    if s is not None:
-                        return s
-                    else:
-                        raise RuntimeError("Unable to determine number of samples.")
-            except Exception:
-                # we need an extra fallback for zarr b/c Julia Zarr and python Zarr
-                # can differ in layout - Julia Zarr does not have to have a group
-                try:
-                    store = zarr.storage.LocalStore(filepath, read_only=True)
-                    arr = zarr.open_array(store, path="adjacency_matrix")
-                    s = max(arr.shape)
                     return s
-                except Exception:
-                    raise
-        else:
-            raise ValueError("mode must be 'hdf5' or 'zarr'")
+                else:
+                    raise RuntimeError("Unable to determine number of samples.")
+        except Exception:
+            # we need an extra fallback for zarr b/c Julia Zarr and python Zarr
+            # can differ in layout - Julia Zarr does not have to have a group
+            try:
+                store = zarr.storage.LocalStore(filepath, read_only=True)
+                arr = zarr.open_array(store, path="adjacency_matrix")
+                s = max(arr.shape)
+                return s
+            except Exception:
+                raise
 
     @property
-    def processed_dir(self) -> str | None:
+    def processed_dir(self) -> str:
         """Get the path to the processed directory.
 
         Returns:
@@ -200,11 +176,7 @@ class QGDatasetBase:
         Returns:
             list[str]: A list of raw file paths.
         """
-        if self.mode == "zarr":
-            suf = ".zarr"
-        else:
-            suf = ".h5"
-
+        suf = ".zarr"
         return [str(Path(f).name) for f in self.input if Path(f).suffix == suf]
 
     @property
@@ -214,71 +186,22 @@ class QGDatasetBase:
         Returns:
             list[str]: A list of processed file paths, excluding JSON files.
         """
-
         if not Path(self.processed_dir).exists():
             return []
+
         return [
             str(f.name)
             for f in Path(self.processed_dir).iterdir()
             if f.is_file() and f.suffix == ".pt" and "data" in f.name
         ]
 
-    def process_chunk_hdf5(
-        self,
-        raw_file: h5py.File,
-        start: int,
-        pre_transform: Callable[[Data | Collection], Data] | None = None,
-        pre_filter: Callable[[Data | Collection], bool] | None = None,
-    ) -> list[Data]:
-        """Process a chunk of data from the raw file. This method is intended to be used in the data loading pipeline to read a chunk of data, apply transformations, and filter the read data, and thus should not be called directly.
-
-        Args:
-            raw_file (h5py.File): The raw HDF5 file to read from.
-            start (int): The starting index of the chunk.
-            pre_transform (Callable[[Data], Data] | None, optional): Transformation that adds additional features to the data. Defaults to None.
-            pre_filter (Callable[[Data], bool] | None, optional): A function that filters the data. Defaults to None.
-
-        Returns:
-            list[Data]: The processed data or None if the chunk is empty.
-        """
-
-        # we can't rely on being able to read from the raw_files in parallel, so we need to read the data sequentially first
-        data = [
-            self.data_reader(
-                raw_file,
-                i,
-                self.float_type,
-                self.int_type,
-                self.validate_data,
-            )
-            for i in range(
-                start, min(start + self.chunksize, raw_file["num_causal_sets"][()])
-            )
-        ]
-
-        def process_item(item):
-            if pre_filter is not None and not pre_filter(item):
-                return None
-            if pre_transform is not None:
-                return pre_transform(item)
-            return item
-
-        results = []
-        if self.n_processes > 1:
-            results = Parallel(n_jobs=self.n_processes)(
-                delayed(process_item)(datapoint) for datapoint in data
-            )
-        else:
-            results = [process_item(datapoint) for datapoint in data]
-        return [res for res in results if res is not None]
-
-    def process_chunk_zarr(
+    def process_chunk(
         self,
         store: zarr.storage.LocalStore,
         start: int,
         pre_transform: Callable[[Data | Collection], Data] | None = None,
         pre_filter: Callable[[Data | Collection], bool] | None = None,
-    ) -> list[Data]:
+    ) -> Sequence[Data]:
         """Process a chunk of data from the raw file. This method is intended to be used in the data loading pipeline to read a chunk of data, apply transformations, and filter the read data, and thus should not be called directly.
 
         Args:
@@ -318,15 +241,3 @@ class QGDatasetBase:
             ]
 
         return [res for res in results if res is not None]
-
-    def process_chunk(
-        self,
-        store: zarr.storage.LocalStore | h5py.File,
-        start: int,
-        pre_transform: Callable[[Data], Data] | None = None,
-        pre_filter: Callable[[Data], bool] | None = None,
-    ):
-        if self.mode == "hdf5":
-            return self.process_chunk_hdf5(store, start, pre_transform, pre_filter)
-        else:
-            return self.process_chunk_zarr(store, start, pre_transform, pre_filter)
