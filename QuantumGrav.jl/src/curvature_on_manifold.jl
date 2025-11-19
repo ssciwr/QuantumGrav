e76a54edfce534d5d74d905532751f740f9a278f
Fabian Wagner <f.wagner@thphys.uni-heidelberg.de>
2025-11-19 12:15:19 +0100

    - fixed bug that caused overflows in computing Chebyshev derivative matrices

--- QuantumGrav.jl/src/curvature_on_manifold.jl
+++ QuantumGrav.jl/src/curvature_on_manifold.jl
@@ -30,21 +30,18 @@ function chebyshev_derivation_matrix(
         throw(ArgumentError("max_cheb_order has to be at least 1, is $(derivative_order)."))
     end
 
-    transformation_matrix = zeros(max_cheb_order + 1, max_cheb_order + 1)
+    transformation_matrix_first_derivative = zeros(max_cheb_order + 1, max_cheb_order + 1)
 
     for n in 0 : max_cheb_order
 
-        for k in mod(n-derivative_order, 2) : 2 : n - derivative_order
+        for k in mod(n-1, 2) : 2 : n - 1
             prefac = k == 0 ? 0.5 : 1
 
-            transformation_matrix[k+1, n+1] += 2^derivative_order * n * prefac * 
-                                                binomial(Int64((n + derivative_order - k) / 2 - 1), Int64((n - derivative_order - k) / 2)) * 
-                                                factorial(Int64((n + derivative_order + k) / 2 - 1)) / 
-                                                factorial(Int64((n - derivative_order + k) / 2))
+            transformation_matrix_first_derivative[k+1, n+1] += 2 * n * prefac
         end
     end
 
-    return transformation_matrix
+    return transformation_matrix_first_derivative^derivative_order
 end
 
 """
