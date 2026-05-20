7c287ea077c5e2915c2f025ed5f65931819afff7
Harald Mack <harald.mack@protonmail.com]>
2026-05-20 14:00:19 +0200

    update exports

--- QuantumGravPy/src/QuantumGrav/__init__.py
+++ QuantumGravPy/src/QuantumGrav/__init__.py
@@ -7,9 +7,9 @@ from .utils import (
 from .dataset_ondisk import QGDataset
 from .gnn_model import GNNModel
 from .evaluate import (
-    DefaultEvaluator,
-    DefaultTester,
-    DefaultValidator,
+    Evaluator,
+    Tester,
+    Validator,
 )
 
 from .config_utils import ConfigHandler, get_loader
@@ -42,9 +42,9 @@ __all__ = [
     "initialize_ddp",
     "cleanup_ddp",
     # evaluation
-    "DefaultEvaluator",
-    "DefaultValidator",
-    "DefaultTester",
+    "Evaluator",
+    "Validator",
+    "Tester",
     "DefaultEarlyStopping",
     # config handler
     "get_loader",
