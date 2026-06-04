fd04a63649796c1606c763c3732dc476c1fe1751
Harald Mack <harald.mack@protonmail.com]>
2026-06-04 17:45:03 +0200

    update dataset to account for changed semantics

--- QuantumGravPy/src/QuantumGrav/dataset_ondisk.py
+++ QuantumGravPy/src/QuantumGrav/dataset_ondisk.py
@@ -123,7 +123,7 @@ class QGDataset(Dataset):
                         mode="r",
                     )
                     n = len(root)
-                    self._num_samples_per_file[filepath] = n
+                    self._num_samples_per_file[str(filepath)] = n
                 self._num_samples += n
 
             Path(self.processed_dir).mkdir(parents=True, exist_ok=True)
@@ -281,7 +281,7 @@ class QGDataset(Dataset):
         original_index = idx
         final_file: Path | str | None = None
 
-        for size, dfile in zip(self._num_samples_per_file, self.input):
+        for dfile, size in self._num_samples_per_file.items():
             if idx < size:
                 final_file = dfile
                 break
@@ -290,7 +290,7 @@ class QGDataset(Dataset):
 
         if final_file is None:
             raise RuntimeError(
-                f"Error, index {original_index} could not be found in the supplied data files of size {self._num_samples_per_file} with total size {self._num_samples}"
+                f"Error, index {original_index} could not be found in the supplied data files of size {list(self._num_samples_per_file.values())} with total size {self._num_samples}"
             )
         return final_file, idx
 
